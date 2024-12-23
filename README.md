ab4e95ebddc85bd6bc28f0b9fd7a75d2133bb404
Cukehater <cukehater@gmail.com>
2024-12-23 12:00:35 +0900

    feat: Create CSS Modules 개발 및 README.md 작성

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Create CSS Modules
+
+A VSCode extension that makes it easy to create CSS Modules files.
+
+## ✨ Key Features
+
+- Automatically creates CSS Modules files with the same name as your current file
+- Flexible directory configuration for file creation
+- Supports various CSS extensions
+  - `css`, `scss`, `sass`, `less`, `styl`, `stylus`
+- Compatible file formats
+  - `js`, `jsx`, `ts`, `tsx`
+
+## ⌨️ Default Keyboard Shortcuts
+
+| Operating System | Shortcut           |
+| ---------------- | ------------------ |
+| Windows, Linux   | `Ctrl + Shift + M` |
+| Mac              | `Cmd + Shift + M`  |
+
+## ⚙️ Configuration
+
+You can configure the following options in VSCode's `settings.json`
+
+```json
+{
+  "createCSSModules.path": "./styles", // Directory path for CSS files
+  "createCSSModules.extension": ".scss" // File extension for CSS files
+}
+```
+
+### Configuration Options
+
+| Option                       | Description                                            | Default |
+| ---------------------------- | ------------------------------------------------------ | ------- |
+| `createCSSModules.path`      | Directory path where CSS Modules files will be created | `./`    |
+| `createCSSModules.extension` | File extension for the generated CSS files             | `.css`  |
+
+## 🎥 Usage Example
+
+![Demo Screen](./demo.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,102 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import fs from 'fs'
+import path from 'path'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// 확장 기능이 활성화될 때 호출되는 함수
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "create-css-modules" is now active!'
-  )
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'create-css-modules.helloWorld',
+  // 'extension.createCSSModules' 명령어 등록
+  const createFileCommand = vscode.commands.registerCommand(
+    'extension.createCSSModules',
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Hello World from Create CSS Modules!'
-      )
+      // 활성화 된 텍스트 에디터가 있는지 확인
+      if (vscode.window.activeTextEditor) {
+        // 현재 열려있는 파일의 폴더 경로
+        const currentDirectory = path.dirname(
+          vscode.window.activeTextEditor.document.fileName
+        )
+
+        // 현재 열려있는 파일의 이름
+        const openedFileName = path.parse(
+          vscode.window.activeTextEditor.document.fileName
+        ).name
+
+        // 현재 열려있는 파일의 확장자
+        const openedFileExtension = path.parse(
+          vscode.window.activeTextEditor.document.fileName
+        ).ext
+
+        // 파일 확장자 검사 (tsx, jsx, ts, js)
+        if (!checkFileExtension(openedFileExtension)) {
+          vscode.window.showErrorMessage(
+            'Files with this extension are not valid for creating CSS Modules files!'
+          )
+          return
+        }
+
+        // 파일 생성 함수 호출
+        createCSSModulesFile(currentDirectory, openedFileName)
+      } else {
+        // 활성화된 에디터가 없을 때 메시지 표시
+        vscode.window.showErrorMessage(
+          'No open editor detected to match filename!'
+        )
+      }
     }
   )
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(createFileCommand)
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+const checkFileExtension = (extension: string): boolean => {
+  if (
+    extension === '.tsx' ||
+    extension === '.jsx' ||
+    extension === '.ts' ||
+    extension === '.js'
+  ) {
+    return true
+  }
+  return false
+}
+
+const createCSSModulesFile = (currentDirectory: string, fileName: string) => {
+  // VSCode 설정에서 지정한 디렉터리 경로 및 파일 확장자 가져오기
+  const config = vscode.workspace.getConfiguration('createCSSModules')
+  const settedFilePath = config.get('path', './') // 기본값: 현재 디렉터리
+  const settedFileExtension = config.get('extension', 'css') // 기본값: .css
+
+  // 생성할 파일 이름
+  const moduleFileName = `${fileName}.module.${settedFileExtension}`
+
+  // 생성할 파일 경로 (현재 파일 경로 + 생성할 파일 경로)
+  const createdFileDirectory = path.join(currentDirectory, settedFilePath)
+
+  // 기존 파일 존재 유무 체크
+  if (!checkFileExists(path.join(createdFileDirectory, moduleFileName))) {
+    return
+  }
+
+  // 디렉토리가 없으면 생성
+  if (!fs.existsSync(createdFileDirectory)) {
+    fs.mkdirSync(createdFileDirectory, { recursive: true })
+  }
+
+  fs.writeFile(path.join(createdFileDirectory, moduleFileName), '', err => {
+    if (err) {
+      return vscode.window.showErrorMessage(
+        'Failed to Create CSS Modules file!'
+      )
+    }
+  })
+
+  vscode.window.showInformationMessage('Successfully created CSS Modules file!')
+}
+
+// 생성할 파일 유무 체크 함수
+const checkFileExists = (filePath: string): boolean => {
+  if (fs.existsSync(filePath)) {
+    vscode.window.showErrorMessage(`File is already exists in this directory.`)
+    return false
+  }
+  return true
+}
